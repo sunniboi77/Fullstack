51ab5f88ed86abd281de1e956b5da190e1538ff1
Attila Szaniszlo <attila.szaniszlo@gmail.com>
2022-12-12 21:02:07 +0100

    node express post

---
+++ index.js
@@ -0,0 +1,9 @@
+var _ = require('underscore');
+//core mudule
+//file or folder in this project
+//3rd step assumes that is in the node_modules folder
+
+let x = _.contains([1, 2, 3], 2);
+
+console.log(x);
+
