2949057ae7f7ec754c59e222a61130253acb9553
Attila Szaniszlo <attila.szaniszlo@gmail.com>
2023-01-21 12:06:32 +0100

    04 building web server

--- 04_Restful/index.js
+++ 04_Restful/index.js
@@ -2,8 +2,12 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req,res)=>{
-    res.send('hello world');
+    res.send('hello world!!!!!!!!!!!!!!!');
 });
+
+app.get('/courses', (req,res)=>{
+    res.send([1,2,3])
+})
 
 app.listen(3000, ()=>console.log('listening on port 3000'));
 
