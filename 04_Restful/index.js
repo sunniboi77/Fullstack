9fe20996da27f6f87e19d92688f71dba2974be30
Attila Szaniszlo <attila.szaniszlo@gmail.com>
2023-01-22 12:07:41 +0100

    08 handling get requests

--- 04_Restful/index.js
+++ 04_Restful/index.js
@@ -1,16 +1,34 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req,res)=>{
-    res.send('hello world!!!!!!!!!!!!!!!');
+app.get('/', (req, res) => {
+    res.send('hello212asdfaf     !')
 });
 
-app.get('/courses', (req,res)=>{
-    res.send([1,2,3])
+app.get('/api/courses', (req, res) => {
+    res.send([1, 2, 3]);
 })
 
-app.listen(3000, ()=>console.log('listening on port 3000'));
+app.get('/api/courses/:id', (req, res) => {
+    res.send(req.params.id)
+})
 
-// app.post();
-// app.put();
-// app.delete();
+//multiple parameters results :
+// {"year":"2022","month":"12"}
+app.get('/api/posts/:year/:month', (req, res) => {
+    res.send(req.params)
+})
+
+//multiple parameters results with "?" mark 
+// query string parameter:?sortBy=name
+// this is optional 
+// http://localhost:3000/api/posts/2022/12?sortBy=mame
+// result is : 
+// {"sortBy":"name"}
+app.get('/api/posts2/:year/:month', (req, res) => {
+    res.send(req.query)
+})
+
+//PORT
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`listening on port ${port}...`));
