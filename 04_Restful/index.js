423cdc329c8c21375b29755f837782a1121f7887
Attila Szaniszlo <attila.szaniszlo@gmail.com>
2023-01-26 11:36:36 +0100

    12 Put request

--- 04_Restful/index.js
+++ 04_Restful/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+const Joi = require('joi');
+const { result } = require("underscore");
 
 app.use(express.json());
 
@@ -34,14 +36,60 @@ app.get('/api/courses/:id', (req, res) => {
 })
 
 app.post('/api/courses', (req, res) => {
+    // const schema = Joi.object({
+    //     name: Joi.string().min(3).required()
+    // });
+
+    // const validation = schema.validate(req.body);
+    // // console.log(validation);
+
+    // if (validation.error) {
+    //     res.status(400).send(validation.error.details[0]);
+    //     return;
+    // }
+
+    const { error } = validateCourse(req.body); //result.error
+    if (error) {
+        res.status(400).send(error.details[0]);
+        return;
+    }
+
     const course = {
-        id: courses.length + 1, 
+        id: courses.length + 1,
         name: req.body.name
     };
     courses.push(course);
-    res.send(course);
+    res.send({ error, course });
 });
-``
+
+app.put('/api/courses/:id', (req, res) => {
+    // look up course
+    // if not existing, return 404
+    const course = courses.find(c => c.id === parseInt(req.params.id))
+    if (!course) res.status(404).send("the course with the given id was not found")
+
+    // validate
+    // if invalid, return 400 - Bad request
+    const { error } = validateCourse(req.body); //result.error
+    if (error) {
+        res.status(400).send(error.details[0]);
+        return;
+    }
+    // update course
+    // return updated course 
+    course.name = req.body.name;
+    res.send(course);
+})
+
+function validateCourse(course) {
+    const schema = Joi.object({
+        name: Joi.string().min(3).required()
+    });
+
+    const validation = schema.validate(course);
+    return validation;
+}
+
 //PORT
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`listening on port ${port}...`));
