23bf596d3bccf5243e21ff3663a55d233dace996
Attila Szaniszlo <attila.szaniszlo@gmail.com>
2023-02-06 12:20:01 +0100

    8-12 Lesson done in separate files

--- 06_Async/index8.js
+++ 06_Async/index8.js
@@ -1,58 +1,34 @@
-// video 5
-//Named functions
-
-const c = require("config");
-
 console.log('Before');
-
-
-// callback hell 
-getUser(1, (user) => {
-    getRepositories(user.gitHubUser, (repos) => {
-        getCommits(repos[0], (commits) => {
-            console.log(commits);
-        })
-    })
-})
-
-//promises flat structure
 getUser(1)
-    .then(user => getRepositories(user.gitHubUser))
-    .then(repos => getCommits(repos[0]))
-    .then(commits => console.log('Commits', commits))
-    .catch(err => console.log('error', err.message));
+    .then(user => getRepositories(user.gitHubUsername))
+    .then(repositories => getCommits(repositories[0]))
+    .then(commits => console.log('Commits:', commits))
+    .catch(err => console.log('Error:', err.message));
+console.log('After');
 
-//async function with PROMISE 
 function getUser(id) {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
-            console.log('reading a user from db...');
-            // this user object is not av ailable at runtime
-            resolve({ id: id, gitHubUser: 'attila' })
-        }, 1000);
+            console.log('Reading a user from a database...');
+            resolve({ id: id, gitHubUsername: 'Zifah' });
+        }, 2000);
     });
 }
 
 function getRepositories(username) {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
-            console.log('reading a repo from repos database ');
-            console.log(username);
-            resolve({ repo: ['repo1', 'repo2', 'repo3'] })
-        }, 300)
-    })
+            console.log(`Reading ${username}'s repositories from GitHub API`);
+            resolve(['repo1', 'repo2', 'repo3']);
+        }, 2000);
+    });
 }
 
-function getCommits(repos) {
+function getCommits(repo) {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
-            console.log('Calling github database..');
-            resolve({ commit: ['commit1', 'commit2', 'commit3'] })
-        }, 400)
-    })
+            console.log(`Fetching GitHub commits on repo: ${repo}`);
+            resolve(['commit1']);
+        }, 2000);
+    });
 }
-
-//callbacks
-//promises
-//async/await
-
