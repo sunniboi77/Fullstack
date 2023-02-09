e9b294b32f2111fb802c1334a5e86775be22ac0b
Attila Szaniszlo <attila.szaniszlo@gmail.com>
2023-02-09 14:04:45 +0100

    8-6 mongoshell imported collection from local db is working

--- 08_Valid/index2.js
+++ 08_Valid/index2.js
@@ -1,17 +1,17 @@
-//  Video 4
 //  Async validator
-
-const { number } = require("joi");
+// const { number } = require(joi);
+// Collection imported in Mongoshell is imported properly 
 const mongoose = require('mongoose');
 
 const startupDebugger = require('debug')('app:startup');
 const dbDebugger = require('debug')('app:db');
+const Schema = mongoose.Schema;
 // run debugger
 // $env:DEBUG='app:db'; node xyz.js
 // $env:DEBUG='app:db';node index2.js
 
 dbDebugger('Connected to database...');
-dbDebugger('Debugger workingmongmo..');
+
 //hardcoded - this returns a promise
 mongoose.connect('mongodb://127.0.0.1:27017/machines')
     .then(() => console.log('connected to MongoDB'))
@@ -31,6 +31,19 @@ const delay = (n) => {
 const cl = (...args) => console.log(...args);
 cl("hello")
 
+//create Schema for imported collection; 
+const tecaMachineSchema = new Schema({
+    id: Number,
+    // NAME: String,
+    // SYSTEM: String,
+    // COLOR: Number,
+    // DRIVE: String,
+    // CYCLES: Number,
+    // Force: Number
+},
+    { collection: 'TecaMachines' }
+);   // collection name
+
 const machineSchema = new mongoose.Schema({
     //validation is only meaningful in mongoose
     //mongodb doesnot have this validation 
@@ -44,7 +57,9 @@ const machineSchema = new mongoose.Schema({
     category: {
         type: String,
         required: true,
-        enum: ['big', 'small', 'medicine']
+        enum: ['pad', 'fast', 'small'],
+        lowercase: true,
+        trim: true //paddings will be removed by mongoose
     },
     manufacturer: String,
     tags: {
@@ -70,29 +85,32 @@ const machineSchema = new mongoose.Schema({
 
     //price is required if machine is standard 
     //note the function cannot be arrow function
-    //because arrow function does not have their own "this"
+    //because arrow function does not have their own this
     //it will refer to the calling functions variables 
     //there is not defined
     price: {
         type: Number,
         required: function () { return this.isStandard },
         min: 10,
-        max: 200
+        max: 200,
+        get: v => Math.round(v),
+        set: v => Math.round(v)
     }
 });
 
 //Classes, objects
 //Course is a Class
-const Machine = mongoose.model('machines', machineSchema)
+const Machine = mongoose.model('machines', machineSchema);
+const TecaMachine = mongoose.model('TecaMachine', tecaMachineSchema);
 
 async function createMachine() {
     const machine = new Machine({
-        name: 'Kent6',
-        category: '-',
+        name: 'Kent9',
+        category: 'PAD',
         manufacturer: 'Kent',
-        tags: [],
+        tags: ["pad"],
         isStandard: true,
-        price: 150
+        price: 150.1
     });
 
     try {
@@ -116,4 +134,37 @@ async function createMachine() {
     }
 }
 
-createMachine();    
+// Uses collection TecaMachine
+async function getMachines() {
+    dbDebugger('query runs');
+
+    const machines2 = await TecaMachine.find()
+    console.log(machines2);
+    // .or([{ manufacturer: 'Kent' }, { manufacturer: 'Morlock' }]);
+}
+
+async function getMachines() {
+    dbDebugger('query runs');
+
+    const machines4 = await Machine
+        // .find()
+        .find({ _id: "63e209e802382954f876e2af" })
+        .sort({ name: 1 })
+        .select({ name: 1, manufacturer: 1 })
+    console.log(machines4[0].name);
+
+    // .or([{ manufacturer: 'Kent' }, { manufacturer: 'Morlock' }]);
+}
+
+// Uses collection machines 
+async function getMachinesCount() {
+    dbDebugger('getMachinesCount query runs');
+    const machines3 = await Machine
+        // .find({ manufacturer: /.*Prin.*/i })
+        .count()
+    console.log('nr of machines found:', machines3);
+}
+
+getMachinesCount()
+// createMachine();
+getMachines()
