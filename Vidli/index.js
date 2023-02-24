d127ffbc77a1afd22406d8238b026f2b14c8b809
Attila Szaniszlo <attila.szaniszlo@gmail.com>
2023-02-24 08:26:32 +0100

    Vidli genres delete endpoint added

--- Vidli/index.js
+++ Vidli/index.js
@@ -4,9 +4,9 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
-const genres = [
-    { id: 1, name: 'Thriller', description: 'Movies that increase the heart rate' },
-    { id: 2, name: 'Rom-Com', description: 'Movies that will make your eyes well up with tears' }
+const genres = [ 
+    { id: 1, name: 'Thriller', description: 'Movies that increase the heart rate'}, 
+    { id: 2, name: 'Rom-Com', description: 'Movies that will make your eyes well up with tears'} 
 ];
 
 app.get('/api/genres', (req, res) => {
@@ -14,11 +14,18 @@ app.get('/api/genres', (req, res) => {
 });
 
 app.get('/api/genres/:id', (req, res) => {
-    const course = genres.find(c => c.id === parseInt(req.params.id));
+    const genre = genres.find(c => c.id === parseInt(req.params.id));
+    if(!genre) return res.status(404).send(`A genre with id ${req.params.id} was not found!`);
+    res.send(genre);
+});
 
-    if (!course) return res.status(404).send(`A course with id ${req.params.id} was not found!`)
 
-    res.send(course);
+app.delete('/api/genres/:id', (req, res) => {
+    const genre = genres.find(c => c.id === parseInt(req.params.id));
+    if(!genre) return res.status(404).send(`A genre with id ${req.params.id} was not found!`);
+    const index = genres.indexOf(genre);
+    genres.splice(index, 1);
+    res.send(genre);
 });
 
 const port = process.env.PORT_NUMBER || 3000;
