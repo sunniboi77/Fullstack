c29b1005667dda60d0e0f831ed83050898655e2f
Attila Szaniszlo <attila.szaniszlo@gmail.com>
2023-02-24 17:58:06 +0100

    add in create genre endpoint

--- Vidli/index.js
+++ Vidli/index.js
@@ -4,9 +4,9 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
-const genres = [ 
-    { id: 1, name: 'Thriller', description: 'Movies that increase the heart rate'}, 
-    { id: 2, name: 'Rom-Com', description: 'Movies that will make your eyes well up with tears'} 
+const genres = [
+    { id: 1, name: 'Thriller', description: 'Movies that increase the heart rate' },
+    { id: 2, name: 'Rom-Com', description: 'Movies that will make your eyes well up with tears' }
 ];
 
 app.get('/api/genres', (req, res) => {
@@ -15,18 +15,45 @@ app.get('/api/genres', (req, res) => {
 
 app.get('/api/genres/:id', (req, res) => {
     const genre = genres.find(c => c.id === parseInt(req.params.id));
-    if(!genre) return res.status(404).send(`A genre with id ${req.params.id} was not found!`);
+    if (!genre) return res.status(404).send(`A genre with id ${req.params.id} was not found!`);
     res.send(genre);
 });
 
 
 app.delete('/api/genres/:id', (req, res) => {
     const genre = genres.find(c => c.id === parseInt(req.params.id));
-    if(!genre) return res.status(404).send(`A genre with id ${req.params.id} was not found!`);
+    if (!genre) return res.status(404).send(`A genre with id ${req.params.id} was not found!`);
     const index = genres.indexOf(genre);
     genres.splice(index, 1);
     res.send(genre);
 });
+
+app.post('/api/genres', (req, res) => {
+    const { error } = validateGenre(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    if (genres.find(g => g.name.toLowerCase() === req.body.name.toLowerCase())) {
+        return res.status(400).send('Another genre with this name already exists');
+    }
+
+    const genre = {
+        id: genres.length + 1,
+        name: req.body.name,
+        description: req.body.description
+    };
+    genres.push(genre);
+    res.send(genre);
+});
+
+
+function validateGenre(genre) {
+    const schema = Joi.object({
+        name: Joi.string().min(3).required(),
+        description: Joi.string()
+    })
+    const validation = schema.validate(genre);
+    return validation;
+}
 
 const port = process.env.PORT_NUMBER || 3000;
 app.listen(port, () => console.log('Listening on port', port, '...'));
