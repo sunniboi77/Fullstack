3b5fec7e0810dfef634af0e3d7456a13e66cf12e
Attila Szaniszlo <attila.szaniszlo@gmail.com>
2023-02-28 15:05:16 +0100

    mongodb integrated in to vidly

--- Vidli/index.js
+++ Vidli/index.js
@@ -2,16 +2,25 @@ const express = require('express');
 const logger = require('./middleware/logger');
 const authentication = require('./middleware/authentication');
 const bodyParser = require('body-parser');
-
+const mongoose = require('mongoose');
 
 const app = express();
 const genres = require('./routes/genres')
+
+
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(logger);
 app.use(authentication);
-app.use('/api/genres', genres);
+app.use('/api/genres', genres.router);
+// app.use('/api/genres', genres);
+
+
+mongoose.connect('mongodb://127.0.0.1:27017/vidli')
+    .then(() => console.log('connected to MongoDB...'))
+    .catch(err => console.log('could not connect'))
+
 
 const port = process.env.PORT_NUMBER || 3000;
-app.listen(port, () => console.log('Listening on port', port, '...'));cd
+app.listen(port, () => console.log('Listening on port', port, '...')); 
